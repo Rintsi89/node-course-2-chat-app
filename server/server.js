36149f58641f5f2f22c1ff7c1c80c058cf900139
Ville <ville.rintala@hotmail.com>
2019-02-14 10:00:23 +0200

    Add newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -17,9 +17,20 @@ console.log(publicPath);
 io.on("connection", (socket) => {
 	console.log("New user connected");
 
+socket.emit("newMessage", {
+	from: "John",
+	text: "See you then",
+	createdAt: 123123
+});
+
+socket.on("createMessage", (message) => {
+	console.log("createMessage", message);
+});
+
 socket.on("disconnect", () => {
 		console.log("Client disconnected");
 	});
+
 });
 
 server.listen(port, () => {
