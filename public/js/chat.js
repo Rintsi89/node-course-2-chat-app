ab19fd2329de1b73aeee773b5d4a705e0622b012
Ville <ville.rintala@hotmail.com>
2019-02-19 08:33:41 +0200

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,11 +19,30 @@ function scrollToBottom () {
 }
 
 socket.on("connect", function () {
-	console.log("Connected to server");
+	var params = jQuery.deparam(window.location.search);
+
+	socket.emit("join", params, function (err) {
+		if (err) {
+			alert(err);
+			window.location.href = "/"; 
+		} else {
+			console.log("No error");
+		}
+	});
 });
 
 socket.on("disconnect", function () {
 	console.log("Disconnected from server");
+});
+
+socket.on("updateUserList", function (users) {
+	var ol = jQuery("<ol></ol>");
+
+	users.forEach(function (user) {
+		ol.append(jQuery("<li></li>").text(user));
+	});
+
+	jQuery("#users").html(ol);
 });
 
 socket.on("newMessage", function (message) {
@@ -65,7 +84,6 @@ jQuery("#message-form").on("submit", function (e) {
 	var messageTextbox = jQuery("[name=message]");
 
 	socket.emit("createMessage", {
-		from: "User",
 		text: messageTextbox.val()
 	}, function () {
 		messageTextbox.val("") // This line clears the message box after send
